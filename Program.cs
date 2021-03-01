735271c882e9dfa80c80472038faca3c1ef2b70a
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-03-01 14:04:32 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures1
+{   public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number x = new Number();
+            x.Amount = 42.06m;
+            Console.WriteLine(x.Amount);
+            Console.ReadLine();
+        }
+    }
+}
